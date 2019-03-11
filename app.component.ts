fc8616882b4e33c839570fb74778df581e0d0213
Emmanuel idehen <emmanuel.idehen@mvsu.edu>
2019-03-11 18:18:42 -0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Emmanuel first app';
+  // how i go take  run am from this Terminal instead of main terminal !
+  // note whitespaces are a terminal  warning and comment must start with a white space! lol
+
+
+
+
+}
